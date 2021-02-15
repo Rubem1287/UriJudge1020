df3c7893fc69ce818252ef5beb64c033f8c7c132
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-15 20:42:00 -0300

    Add files via upload

---
+++ uri 1020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor,anos, meses,dias,resto;
+
+
+            Console.WriteLine();
+            Console.Write("digite um valor para ser convertido em anos, meses e dias : ");
+            valor = int.Parse(Console.ReadLine());
+
+            anos = valor / 365;
+            Console.WriteLine();
+            Console.Write(anos+" ano(s)");
+            resto = valor % 365;
+
+            meses = resto / 30;
+            Console.WriteLine();
+            Console.Write(meses + " mes(es)");
+            dias = resto % 30;
+
+            Console.WriteLine();
+            Console.Write(dias + " dia(s)");
+
+        }
+    }
+}
